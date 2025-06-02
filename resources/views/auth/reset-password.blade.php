74ef26363895eaea0ed74346da4504ee42aa0415
Kira <117554978+kiraware@users.noreply.github.com>
2025-06-02 09:38:51 +0700

    run "npx @tailwindcss/upgrade"

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,5 +1,5 @@
 <x-guest-layout>
-    <div class="min-h-screen flex items-center justify-center bg-gradient-to-br from-[#272A31] to-[#101015]">
+    <div class="min-h-screen flex items-center justify-center bg-linear-to-br from-[#272A31] to-[#101015]">
         <div class="flex text-white overflow-hidden w-full max-w-4xl">
             <!-- back button -->
             <a href="{{ route('login') }}"
