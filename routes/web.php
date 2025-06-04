eb18376015fc37f7d55605be755e2390b0228190
kiraware <117554978+kiraware@users.noreply.github.com>
2025-06-04 11:20:59 +0700

    fix homepage and public footer (#17)

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,14 @@
 <?php
 
 use App\Http\Controllers\CourseController;
-use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LessonController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Middleware\Admin;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
