aa649030593b1a4646a15fe2e5b016fdbaef70d6
iqbalanandra <112565226+iqbalanandra@users.noreply.github.com>
2025-06-08 02:50:42 +0700

    Fixing Navbar and Footer (#18)
    Changing navbar and footer into components and minor fix on
    welcome.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
 </head>
 
 <body class="font-sans antialiased">
-    <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+    <div class="min-h-screen bg-gray-100 dark:bg-gray-950">
         @include('layouts.navigation')
 
         <!-- Page Heading -->
@@ -34,6 +34,7 @@
             {{ $slot }}
         </main>
     </div>
+    <x-footer />
 </body>
 
 </html>
